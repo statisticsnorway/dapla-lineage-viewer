a274a7257d40488b7ca5db741417d508c4e12be6
SjurSutterudSagen <sjs@ssb.no>
2021-03-05 14:28:43 +0100

    Update @beam-australia/react-env to version 3.0.8 (#15)

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,6 @@ import '@testing-library/jest-dom/extend-expect'
 jest.mock('axios-hooks')
 jest.mock('graphql-hooks')
 
-window._env = {
+window.__ENV = {
   REACT_APP_API: process.env.REACT_APP_API
 }
